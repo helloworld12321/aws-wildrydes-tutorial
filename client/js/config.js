07e47bcbb40c8c74e222c0f58a0a8210d1d58514
Joe Walbran <walbr037@morris.umn.edu>
2021-04-03 18:44:41 -0500

    Use our own Cognito User Pool + API Gateway
    Instead of @NicMcPhee's. (This repository was pre-configured to use
    Professor Nic's services--now that we've made our own Cognito User Pool
    and API Gateway, we can point the web app to those instead.)

--- client/js/config.js
+++ client/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NHM2B0c8z', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '202tcvo9rmgo1qudb4d0p66gqv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_3SDPb37KI',
+        userPoolClientId: 'pkl181ah9eqh4e8jsi2q01vqc',
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: 'https://puby08jq6k.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://0ljmg4zxk6.execute-api.us-east-1.amazonaws.com/prod'
     }
 };
